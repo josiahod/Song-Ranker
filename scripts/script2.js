d3486a1027b14be0b2b0d20796d428c14b2a71b6
josiahod <115951301+josiahod@users.noreply.github.com>
2023-01-02 21:28:42 -0500

    Update script2.js

--- scripts/script2.js
+++ scripts/script2.js
@@ -5,7 +5,7 @@ var albumList = new Array();
  
  async function load() 
     {
-      var url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artist + "&autocorrect=1&api_key=" + apikey + "&format=json";
+      var url = "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artist + "&autocorrect=1&api_key=" + apikey + "&format=json";
       let obj = await (await fetch(url)).json();
       var namMember = new Array();
       for (var i = 0; (i < obj.topalbums.album.length) && (i < 20); i++) 
