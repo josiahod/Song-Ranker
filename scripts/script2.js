799bb3f1fb57e689016bf94aa72ef01d888f71af
josiahod <115951301+josiahod@users.noreply.github.com>
2023-01-03 23:30:53 -0500

    Update script2.js

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,21 +1,47 @@
 var apikey = "9f3a144b762612624edfc59b0e3451ad";
 var artist = prompt("artist name");
-var album = "melodrama" //prompt("album name");
 var albumList = new Array();
- 
- async function load() 
+
+
+
+ async function loadThis(changedArtist) 
     {
-      var url = "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artist + "&autocorrect=1&api_key=" + apikey + "&format=json";
+      
+
+
+      albumList = [];
+      var url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + changedArtist + "&autocorrect=1&api_key=" + apikey + "&format=json";
       let obj = await (await fetch(url)).json();
       var namMember = new Array();
       for (var i = 0; (i < obj.topalbums.album.length) && (i < 20); i++) 
       {
         albumList.push(obj.topalbums.album[i].name) 
       }
-      console.log(albumList);
+      const artNames = document.getElementById("artNames");
+      artNames.dataset.artist = obj.topalbums.album[0].artist.name;
+
+      var objTo2 = document.getElementById('AlbumName');
+      objTo2.textContent = "Select " + artNames.dataset.artist + " Album To Rank";
+
 
       var select = document.getElementById("selectNumber");
 
+      function removeOptions(selectElement) 
+      {
+        var i, L = selectElement.options.length - 1;
+        for(i = L; i >= 0; i--) {
+           selectElement.remove(i);
+        }
+     }
+
+     removeOptions(select);
+
+     var opt = "Pick An Album";
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+     
       for(var i = 0; i < albumList.length; i++) 
       {
         var opt = albumList[i];
@@ -25,7 +51,14 @@ var albumList = new Array();
         select.appendChild(el);
       }
 
-
     }
 
-    load();
+    loadThis(artist);
+
+
+    var buttonArtist = document.getElementById("ArtistButton");
+    buttonArtist.addEventListener('click', function() 
+    { 
+     changedArtist = prompt("Pick A New Artist");
+     loadThis(changedArtist);
+    }, false);
