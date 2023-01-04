35f7fde25549eee04c3522669d40f9c41a5a46a2
josiahod <115951301+josiahod@users.noreply.github.com>
2023-01-03 23:57:39 -0500

    Update myscripts.js

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -473,7 +473,9 @@
             
             counter = 1;
             var elem = document.getElementById("myBar");
+            console.log(elem.style.width)
             var width2 = parseFloat(elem.style.width) 
+            console.log(width2, PostPercent);
             var width = width2;
             var id = setInterval(frame, 25);
             function frame()
@@ -482,6 +484,7 @@
               {
                 clearInterval(id);
                 counter = 0;
+                console.log("executed");
               } 
               else 
               {
